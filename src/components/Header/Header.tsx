1a30753eef799e080e5205960b5dc3fca4a31753
Yury Barysevich <matthias@google.com>
2022-04-03 01:53:57 +0300

    feat: add header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+interface IHeader {
+  currentPage?: string;
+}
+
+export default class Header extends Component<IHeader> {
+  render() {
+    const { currentPage = "currentPage" } = this.props;
+    return (
+      <header className="container mx-auto flex  bg-gray-300 p-4">
+        <div className="font-bold font-sans text-3xl">LOGO</div>
+        <div className="flex items-center ml-auto mr-8">
+          You On: {currentPage} Page Now
+        </div>
+        <div className="flex items-center">
+          <ul className="flex space-x-4 font-bold">
+            <li>
+              <Link to="/">Main Page</Link>
+            </li>
+            <li>
+              <Link to="/aboutus">About Us</Link>
+            </li>
+          </ul>
+        </div>
+      </header>
+    );
+  }
+}
