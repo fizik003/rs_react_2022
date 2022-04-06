7fa47e3a46f70d6c684e0d27d53d3edf29fec6d1
Yury Barysevich <matthias@google.com>
2022-04-06 23:32:35 +0300

    feat:add tests for cards list

---
+++ src/pages/Main/components/CardsList/CardsList.test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { cardsMock } from "mocks";
+import { CardsList } from "./CardsList";
+
+describe("CardsList", () => {
+  it("should show all cards", () => {
+    const currentAmountCards = cardsMock.length;
+    const { container } = render(<CardsList cards={cardsMock} />);
+    expect(container.firstChild?.childNodes.length).toBe(currentAmountCards);
+  });
+});
